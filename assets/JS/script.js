3bdd290b3705e2fb9b5bdc90c787004394549cdb
Shahad-alsaqqaf <98390909+Shahad-alsaqqaf@users.noreply.github.com>
2022-09-22 22:13:13 +0400

    add the website into the repo

---
+++ assets/JS/script.js
@@ -0,0 +1,63 @@
+/* show full screen image on click (For behind the scene images) */
+
+/* For image 1 */
+    var elem = document.getElementById("img-1");
+    function openFullscreen() {
+    if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+} else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+} else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+}
+}
+
+/* For image 2 */
+    var elem2 = document.getElementById("img-2");
+    function openFullscreen2() {
+    if (elem2.requestFullscreen) {
+    elem2.requestFullscreen();
+} else if (elem2.webkitRequestFullscreen) {
+    elem2.webkitRequestFullscreen();
+} else if (elem2.msRequestFullscreen) {
+    elem2.msRequestFullscreen();
+}
+}
+
+/* For image 3 */
+
+    var elem3 = document.getElementById("img-3");
+    function openFullscreen3() {
+    if (elem3.requestFullscreen) {
+    elem3.requestFullscreen();
+} else if (elem3.webkitRequestFullscreen) {
+    elem3.webkitRequestFullscreen();
+} else if (elem3.msRequestFullscreen) {
+    elem3.msRequestFullscreen();
+}
+}
+
+/* For image 4 */
+    var elem4 = document.getElementById("img-4");
+    function openFullscreen4() {
+    if (elem4.requestFullscreen) {
+    elem4.requestFullscreen();
+} else if (elem4.webkitRequestFullscreen) {
+    elem4.webkitRequestFullscreen();
+} else if (elem4.msRequestFullscreen) {
+    elem4.msRequestFullscreen();
+}
+}
+
+/* For image 5 */
+
+    var elem5 = document.getElementById("img-5");
+    function openFullscreen5() {
+    if (elem5.requestFullscreen) {
+    elem5.requestFullscreen();
+} else if (elem5.webkitRequestFullscreen) {
+    elem5.webkitRequestFullscreen();
+} else if (elem5.msRequestFullscreen) {
+    elem5.msRequestFullscreen();
+}
+}
